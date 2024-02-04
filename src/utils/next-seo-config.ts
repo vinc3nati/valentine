fac50548531752ef9b5fb9de69373236b04422d0
vinc3nati <vinitkansend@gmail.com>
2024-02-04 12:15:30 +0530

    improvement:updated url

--- src/utils/next-seo-config.ts
+++ src/utils/next-seo-config.ts
@@ -1,12 +1,12 @@
 export const NEXT_SEO_DEFAULT = {
-    title: 'Be My Better Half',
-    description: 'Tap to see the magic!',
-    openGraph: {
-      type: 'website',
-      locale: 'en_IE',
-      url: 'https://bemyhalf.vercel.app/',
-      title: 'Be My Better Half',
-      description: 'Tap to see the magic!',
-      siteName: 'BeMyBetterHalf',
-    },
-  };
+  title: "Be My Better Half",
+  description: "Tap to see the magic!",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://bemybetterhalf.vercel.app/",
+    title: "Be My Better Half",
+    description: "Tap to see the magic!",
+    siteName: "Be My Better Half",
+  },
+};
